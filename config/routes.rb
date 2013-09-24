61fed6792a76d56f48a3a74b149f033c0f378218
q45 <quintinsmith@icloud.com>
2013-09-24 15:22:05 -0600

    add the ability to log in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Ribbit::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
+  #get "sessions/new"
+  #get "sessions/create"
+  #get "sessions/destroy"
+
+  resources :sessions
   resources :users
+
+  get 'logout', to: 'sessions#destroy', as: 'logout' 
 
   root to: "users#new"
 
