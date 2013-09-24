cb4f7b54cb4e415d777a443c05abe1a582064743
q45 <quintinsmith@icloud.com>
2013-09-24 15:45:06 -0600

    Saving Ribbits

--- app/controllers/ribs_controller.rb
+++ app/controllers/ribs_controller.rb
@@ -1,4 +1,16 @@
 class RibsController < ApplicationController
+	def create
+		rib = Rib.new(rib_params)
+		rib.user_id = current_user.id
+			
+			flash[:error] = "Your Rib was over 140 characters" unless rib.save
+			redirect_to root_url
+	end
 
-	
+	private
+
+	def rib_params
+		params.require(:rib).permit!
+	end
+
 end
