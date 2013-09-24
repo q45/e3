a26292754cd051473c71bc9afcf3b0e65c95153c
q45 <quintinsmith@icloud.com>
2013-09-24 16:42:33 -0600

    add follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@rib = Rib.new
+		@relationship = Relationship.new
 	end
 
 	def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
 
 	has_many :ribs
 
+	has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'followed_id'
+	has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'follower_id'
+	has_many :followers, through: :follower_relationships
+	has_many :followeds, through: :followed_relationships
+
 	before_validation :prep_email
 	before_save :create_avatar_url
 
@@ -11,6 +16,13 @@ class User < ActiveRecord::Base
 	validates :username, presence:true, uniqueness: true
 	validates :name, presence: true
 
+	def following? user
+		self.followeds.include? user
+	end
+
+	def follow user
+		Relation.create follower_id: self.id, followed_id: user.id
+	end
 
 	private
 		def prep_email
